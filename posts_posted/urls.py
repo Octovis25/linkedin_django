be63e7eb0718a6e5426b6d8d3bc6af1458656e7b
Octovis25 <ortrud.junker-wolf@octotrial.com>
2026-04-11 10:05:47 +0000

    fix: urls.py pk type (string statt int)

--- posts_posted/urls.py
+++ posts_posted/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+
 app_name = "posts_posted"
-urlpatterns = [path("", views.post_list, name="list"), path("add/", views.post_add, name="add"),
-    path("<int:pk>/edit/", views.post_edit, name="edit"), path("<int:pk>/delete/", views.post_delete, name="delete")]
+
+urlpatterns = [
+    path("", views.post_list, name="list"),
+    path("add/", views.post_add, name="add"),
+    path("<path:pk>/edit/", views.post_edit, name="edit"),
+    path("<path:pk>/delete/", views.post_delete, name="delete"),
+]
