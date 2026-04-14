4f5362122e27b4e3cbe730ca336e03d6b53241f7
Octovis25 <ortrud.junker-wolf@octotrial.com>
2026-04-14 15:24:25 +0000

    posts_posted: urls.py bereinigt, image_proxy entfernt

--- posts_posted/urls.py
+++ posts_posted/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'posts_posted'
+app_name = "posts_posted"
 
 urlpatterns = [
-    path('', views.post_list, name='list'),
-    path('<path:pk>/edit/', views.post_edit, name='edit'),
-    path('<path:pk>/image/', views.post_image_proxy, name='image_proxy'),
+    path("", views.post_list, name="list"),
+    path("add/", views.post_add, name="add"),
+    path("<int:pk>/edit/", views.post_edit, name="edit"),
+    path("<int:pk>/delete/", views.post_delete, name="delete"),
 ]
