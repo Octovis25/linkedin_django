79477bcf1d04c09257a80332489d1cbcf1ce92a1
Octovis25 <ortrud.junker-wolf@octotrial.com>
2026-04-16 10:41:13 +0000

    fix: WhiteNoise für Static Files auf Render

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -47,3 +47,6 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "contact@octotrial.com")
 DASHBOARD_URL = os.getenv("DASHBOARD_URL", "http://localhost:8000")
 CSRF_TRUSTED_ORIGINS = ["https://linkedin-django-wd-a.onrender.com", "https://*.onrender.com"]
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
