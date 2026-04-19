89b9bf9f46d7fe72cc15dc3013603a6249f20822
Octovis25 <ortrud.junker-wolf@octotrial.com>
2026-04-19 15:29:35 +0000

    fix: add posts and post-image urls

--- linkedin_statistics/stat_urls.py
+++ linkedin_statistics/stat_urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import stat_views
-
 app_name = 'linkedin_statistics'
-
 urlpatterns = [
-    path('',                        stat_views.overview,         name='overview'),
-    path('timeline/',               stat_views.timeline,         name='timeline'),
-    path('timeline/<str:post_id>/', stat_views.timeline_detail,  name='timeline_detail'),
+    path('',                          stat_views.overview,       name='overview'),
+    path('timeline/',                 stat_views.timeline,       name='timeline'),
+    path('timeline/<str:post_id>/',   stat_views.timeline_detail,name='timeline_detail'),
+    path('posts/',                    stat_views.posts,          name='posts'),
+    path('post-image/<str:post_id>/', stat_views.post_image,    name='post_image'),
 ]
